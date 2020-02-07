c0392861837c186c92a6631659c6d90aca8bd93e
Frooozen Braaain <bolod022@gmail.com>
2020-02-08 00:32:23 +0600

    8.2.2020

--- app/src/main/java/com/naimur978/trackyourbuddy/trackyourbuddy/fragment/FriendsFragment.java
+++ app/src/main/java/com/naimur978/trackyourbuddy/trackyourbuddy/fragment/FriendsFragment.java
@@ -14,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -61,6 +62,8 @@ public class FriendsFragment extends Fragment {
 
     private ArrayList<Contact> mContacts;
 
+    private Button showAll;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,7 +74,9 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        showAll =view.findViewById(R.id.showAll);
+        return view;
     }
 
     @Override
@@ -104,6 +109,15 @@ public class FriendsFragment extends Fragment {
                 Intent intent = new Intent(getActivity(), FriendLocationActivity.class);
                 intent.putExtra("mobile_no", holder.mTextViewMobileNo.getText().toString());
                 intent.putExtra("name", holder.mTextViewName.getText().toString());
+                getActivity().startActivity(intent);
+            }
+        });
+
+        showAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), AllFriendsLocationActivity.class);
+                intent.putExtra("contacts_list", mContacts);
                 getActivity().startActivity(intent);
             }
         });
@@ -183,7 +197,7 @@ public class FriendsFragment extends Fragment {
         return false;
     }
 
-    @Override
+  /*  @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.friends_fragment_menu, menu);
@@ -202,5 +216,5 @@ public class FriendsFragment extends Fragment {
                 return super.onOptionsItemSelected(item);
         }
 
-    }
+    }*/
 }
